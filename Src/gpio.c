bb1ea81f886e9bceb1e48f850cfa6b0f1473ecbb
wlzkstlz <543912140@qq.com>
2020-03-18 14:54:53 +0800

    将pwm输出io改为高频

--- Src/gpio.c
+++ Src/gpio.c
@@ -57,14 +57,14 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Pin = PWM_AN_Pin|PWM_BN_Pin|PWM_CN_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PAPin PAPin PAPin */
   GPIO_InitStruct.Pin = PWM_AP_Pin|PWM_BP_Pin|PWM_CP_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /*Configure GPIO pin : PtPin */
